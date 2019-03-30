702cbdcddb74fcae11e7b2116fd12dc3591ed312
larsr <Lars.Rasmusson@gmail.com>
2019-03-31 00:22:45 +0100

    Update animation.py

--- Demo/animation.py
+++ Demo/animation.py
@@ -100,8 +100,7 @@ urls = ['https://www.youtube.com/watch?v=X0vK_57vQ7s',
 
 streams = [StreamLoader(url) for url in urls]
 processes = [threading.Thread(target=s.fetchloop) for s in streams]
-for p in processes: 
-p.start()
+for p in processes: p.start()
 
 t0 = time.time()
 next_frame = t0 
