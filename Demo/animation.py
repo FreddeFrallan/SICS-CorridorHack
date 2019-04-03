85652c2b255e8e14bc9d91705a54e0229a2ae3c4
larsr <Lars.Rasmusson@gmail.com>
2019-04-03 07:33:31 +0200

    read using ffmpeg instead of cv2

--- Demo/animation.py
+++ Demo/animation.py
@@ -3,6 +3,7 @@ import cv2
 import pafy
 import time
 import threading
+import vstream
 
 
 def linear(dx, lt): return dx * lt # 0 <= lt <= 1
@@ -57,8 +58,11 @@ def merge_images(t, imgs):
     a = np.zeros((h, w,3))
 
     for i, img in enumerate(imgs):
-        (x0, y0), (x1, y1) = ul(t - 5 * i).astype(int), lr(t - 5 * i).astype(int)
-        a[y0:y1, x0:x1] = cv2.resize(img.astype(float), (x1-x0,y1-y0))
+        try:
+            (x0, y0), (x1, y1) = ul(t - 5 * i).astype(int), lr(t - 5 * i).astype(int)
+            a[y0:y1, x0:x1] = cv2.resize(img.astype(float), (x1-x0,y1-y0))
+        except:
+            print(f'bad image {i}, {img.shape}')
 
     return a/255
 
@@ -75,32 +79,31 @@ class StreamLoader():
         self.c = cv2.VideoCapture(url)
         self.fps = self.c.get(cv2.CAP_PROP_FPS)
         self.img = self.c.read()[1]
-        self.next_frame = time.time()
-        self.buf = [self.img]
-
+        self.buf = [np.zeros((10,10,3))]
+        print(f'my fps is {self.fps}')
+        if self.fps == 0:
+            self.fps == 15
 
     def fetchloop(self):
-        nr = 0
+        t0 = next_frame = time.time()
+        
         while True:
-            nr += 1
-            if self.url == 0:
-                time.sleep(max(0, self.next_frame - time.time()))
+            time.sleep(max(0, next_frame - time.time()))
+            next_frame += 1/self.fps
             ok, img = self.c.read()
             if ok:
                 self.buf.append(img)
 
-
     def updateloop(self):
+        self.next_frame = time.time()
         while True:
             time.sleep(max(0, self.next_frame - time.time()))
-            if len(self.buf) > 0:
-                self.img = self.buf[0]
+            if len(self.buf) > 1:
                 del self.buf[0]
             self.next_frame += 1 / self.fps
 
     def read(self):
-        return self.img
-
+        return self.buf[0]
 
 
 urls = ['https://www.youtube.com/watch?v=X0vK_57vQ7s',
@@ -108,16 +111,21 @@ urls = ['https://www.youtube.com/watch?v=X0vK_57vQ7s',
         'https://www.youtube.com/watch?v=rWX6BuHUSdM',
         'https://www.youtube.com/watch?v=Nxs53pkE2TY',
         'https://www.youtube.com/watch?v=DBd560d1DIM',
-        'https://www.youtube.com/watch?v=sX1Y2JMK6g8',
+        #'https://www.youtube.com/watch?v=sX1Y2JMK6g8',
         'https://www.youtube.com/watch?v=JqUREqYduHw',
         0]
 
-streams = [StreamLoader(url) for url in urls]
-processes = [threading.Thread(target=s.fetchloop, daemon=True) for s in streams]
-processes += [threading.Thread(target=s.updateloop, daemon=True) for s in streams]
+cam_stream = 'rtsp://192.168.0.142:7447/5ca3cbe70c0af3aa8fbeab20_2'
+cs = StreamLoader(cam_stream)
+p1 = threading.Thread(target=cs.fetchloop, daemon=True)
+p2 = threading.Thread(target=cs.updateloop, daemon=True)
+p1.start()
+p2.start()
 
-for p in processes: 
-    p.start()
+
+bs = [vstream.Buf(u) for u in urls if 'youtube' in str(u)]
+for b in bs: b.start()
+
 
 t0 = time.time()
 next_frame = t0 
@@ -127,16 +135,21 @@ cv2.namedWindow("window", cv2.WND_PROP_FULLSCREEN)
 cv2.setWindowProperty("window",cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
 
 while not done:
-    fps = 20
-    imgs = [s.read() for s in streams]
-    imgs = (imgs + imgs)[:8]
+    fps = 30
+    imgs = [b.buf[0] for b in bs if len(b.buf)>0]
+    imgs.append(cs.read())
+    imgs += [np.ones((10,10,3))*30*(i+1) for i in range(8)]
+    imgs = imgs[:8]
     t = time.time() - t0
-    img = merge_images(t/2, imgs)
+    img = merge_images(t/3, imgs)
     cv2.imshow('window', img)
     next_frame += 1/fps
-
+    for b in bs+[cs]: print(f'{len(b.buf):-4d}, ', end='')
+    print()
     x = cv2.waitKey(max(1, int(1000*(next_frame - time.time()))))
     if x == ord('q'):
         done = True
         cv2.destroyAllWindows()
         break
+
+for b in bs: b.stop()
